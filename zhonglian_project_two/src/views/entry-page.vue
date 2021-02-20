6cb5d4ac16f6848787397fc9699b8c679bd64669
yangqing0516 <15803217937@163.com>
2021-02-21 00:53:07 +0800

    打包，入口文件删除console，改成动态获取识别码

--- zhonglian_project_two/src/views/entry-page.vue
+++ zhonglian_project_two/src/views/entry-page.vue
@@ -57,13 +57,10 @@ export default {
         }
     },
     mounted(){
-        // let url = window.location.href;
-        // console.log('url地址----', url)
-        // let id = url.split("sx_id=")[1];
-        // console.log('事项id-----', id)
-        // localStorage.setItem('sx_id', id)
-        
-        localStorage.setItem('sx_id', "1333344778143465474");
+        let url = window.location.href;
+        let id = url.split("sx_id=")[1];
+        localStorage.setItem('sx_id', id)
+        // localStorage.setItem('sx_id', "1333344778143465474");
         this.onaxios();
     },
     methods: {
